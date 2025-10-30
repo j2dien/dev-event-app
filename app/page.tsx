e28d302323dbae26659ed10845b35dad8307cfd1
j2dien <129026502+j2dien@users.noreply.github.com>
2025-10-30 16:53:19 +0700

    Add ExploreBtn component and update page layout with event details

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import ExploreBtn from "@/components/ExploreBtn";
 import React from "react";
 
 export default function Page() {
@@ -6,6 +7,15 @@ export default function Page() {
       <h1 className="text-center">
         The Hub for Every Developer <br /> Event You Mustn&lsquo;t Miss
       </h1>
+      <p className="text-center mt-5">
+        Hackathons, Meetups, and Conferences, All in One Place
+      </p>
+
+      <ExploreBtn />
+
+      <div className="mt-20 space-y-7">
+        <h3>Featured Events</h3>
+      </div>
     </section>
   );
 }
